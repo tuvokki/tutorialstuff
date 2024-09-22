86e7803b408b045c20c4dc5225b4357bb943f005
Wouter Roosendaal <wouterroosendaal@gmail.com>
2024-09-22 16:21:42 +0200

    Hello with for loop

--- hello.c
+++ hello.c
@@ -1,8 +1,18 @@
-/* Hello world program */
-
 #include <stdio.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");  // Actually do the work here
+    int x = 3, y = 0;
+    // If x > 10, add 17 to y. Otherwise add 37 to y.
+    y += x > 10? 17: 37;
+
+    float f = 3.14;
+    char *s = "Hello, world!";  // char * ("char pointer") is the string type
+
+    printf("%s  i = %d and f = %f!\n", s, y, f);
+
+    int i,j;
+    for (i = 0, j = 10; i < 100; i++, j++) {
+        printf("%d, %d\n", i, j);
+    }
 }
